2fe2cd31806592d88f8ccfbbf9c39c7d5920d4fc
Paolo Oliveira <paolocmo@gmail.com>
2020-02-10 22:58:59 -0300

    base code

---
+++ build.sbt
@@ -0,0 +1,23 @@
+name := "test-api"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+val akkaVersion = "2.5.13"
+val akkaHttpVersion = "10.1.3"
+val circeVersion = "0.9.3"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "io.spray" %%  "spray-json" % "1.3.4",
+
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
+  "de.heikoseeberger" %% "akka-http-circe" % "1.21.0",
+
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,48 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse}
+import akka.http.scaladsl.model.ContentTypes
+
+import spray.json._
+
+import scala.concurrent.Await
+import scala.util.{Failure, Success, Try}
+
+object Main extends App {
+
+  val host = "0.0.0.0"
+  val port = Try(System.getenv("PORT")).map(_.toInt).getOrElse(9000)
+  val c_id = scala.util.Properties.envOrElse("COMMIT", "-none-")
+
+  implicit val system: ActorSystem = ActorSystem(name = "testapi")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  import system.dispatcher
+
+  import akka.http.scaladsl.server.Directives._
+  def route = pathPrefix("v1") {
+    pathEndOrSingleSlash {
+      get {
+        complete(HttpEntity(ContentTypes.`application/json`,  "{\"status\":\"ok\"}"))
+      }
+    } ~ path("version") {
+        get {
+          complete(HttpEntity(ContentTypes.`application/json`, "{\"status\":\"ok\", \"version\":1.0, \"commit\":\"" + c_id + "\"}"))
+        }
+    } ~ path("delay" / Segment) { delay =>
+      get {
+        Thread.sleep(delay.toInt * 1000)
+        complete(HttpEntity(ContentTypes.`application/json`, "{\"status\":\"ok\", \"delay\":\"" + delay + "sec\"}"))
+      }
+    }
+  }
+
+  val binding = Http().bindAndHandle(route, host, port)
+  binding.onComplete {
+    case Success(_) => println("[+] server up!")
+    case Failure(error) => println(s"[-] server error: ${error.getMessage}")
+  }
+
+  import scala.concurrent.duration._
+  Await.result(binding, 3.seconds)
+}
